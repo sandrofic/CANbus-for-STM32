7a929c67730b8474ac2d49f385dc02bdedb6538b
Alessandro Ficarra <102920534+sandrofic@users.noreply.github.com>
2022-06-06 13:17:04 +0200

    Update rx.cpp

--- rx.cpp
+++ rx.cpp
@@ -1,26 +1,21 @@
 #include "mbed.h"
 #include "CAN.h"
 
-//TX
-using ThisThread::sleep_for;
 
-CAN can2(D9, D10);
-DigitalOut led1(LED1); 
-Serial pc(USBTX, USBRX);
+CAN can1(PD_0, PD_1);
+
 int main() {
 
-    pc.baud(9600);
-    can2.frequency(125000);
+    can2.frequency(500000);
     CANMessage msg;
     
     while(1) {
             ////
         printf("Invio messaggio:");
         if (can2.read(msg)) {
-            pc.printf("Messaggio ricevuto: %c ", msg.data[0]);
-            led1 = 1;
+            printf("Messaggio ricevuto: %c ", msg.data[0]);
+
         } 
-    led1=0;
     ThisThread::sleep_for(1000);
     }
 }
